1ee729bbf4616546a7cb9454dfbe75371b291ce6
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-05-22 16:45:05 +0100

    fix vexxing parse warning

--- cppcia/src/ch02/listing_2.6.cpp
+++ cppcia/src/ch02/listing_2.6.cpp
@@ -45,8 +45,8 @@ void do_something_in_current_thread()
 void f()
 {
     int some_local_state;
-    scoped_thread t(std::thread(func(some_local_state)));
-        
+    scoped_thread t{std::thread(func(some_local_state))};
+
     do_something_in_current_thread();
 }
 
